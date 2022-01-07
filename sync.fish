ac31077c23cdfc93a8c54cec0c35706960674348
Kid <kidonng@gmail.com>
2022-01-08 02:33:38 +0800

    Create from scratch on every run

--- sync.fish
+++ sync.fish
@@ -5,13 +5,14 @@ set -l repo https://github.com/fish-shell/fish-shell
 git clone --depth 1 $repo $fish
 set -l commit (git -C $fish rev-parse HEAD)
 
-mkdir -p completions
+mv completions{,.old}
+mkdir completions
 
 for completion in $share/completions/*.fish
     set -l name (basename $completion)
     # Skip if the completion is for builtins or has a corresponding vendored function
     if ! contains $name (builtin -n) && ! test -f $share/functions/$name
-        cp -f $completion completions/
+        cp $completion completions
     end
 end
 
@@ -20,7 +21,7 @@ if string length (git status --porcelain)
     git config user.name "github-actions[bot]"
     git config user.email "github-actions[bot]@users.noreply.github.com"
 
-    git add .
+    git add completions
     git commit -m "Sync with $repo/commit/$commit"
     git push
 end
