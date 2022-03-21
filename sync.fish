2b8ffd7b894be25a3525d7484097cf09194587ce
Kid <44045911+kidonng@users.noreply.github.com>
2022-03-22 00:44:04 +0800

    Skip placeholders

--- sync.fish
+++ sync.fish
@@ -8,10 +8,15 @@ set -l commit (git -C $fish rev-parse HEAD)
 mv completions{,.old}
 mkdir completions
 
+# Builtins should always use vendored completions
+set -l skip (builtin -n)
+# These are placeholders and will invalidate actual completions
+set -a skip brew docker
+
 for completion in $share/completions/*.fish
     set -l name (basename $completion)
-    # Skip if the completion is for builtins or has a corresponding vendored function
-    if ! contains (string replace .fish "" $name) (builtin -n) && ! test -f $share/functions/$name
+    # Skip if the completion is in skip list or has a corresponding vendored function
+    if ! contains (string replace .fish "" $name) $skip && ! test -f $share/functions/$name
         cp $completion completions
     end
 end
