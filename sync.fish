15fcfb374d9630162119bf2946e0a57f0f34974b
Kid <hi@xuann.wang>
2022-08-18 21:12:25 +0800

    Use long options

--- sync.fish
+++ sync.fish
@@ -1,23 +1,23 @@
-set -l fish (mktemp -d)
-set -l share $fish/share
-set -l repo https://github.com/fish-shell/fish-shell
+set --local fish (mktemp -d)
+set --local share $fish/share
+set --local repo https://github.com/fish-shell/fish-shell
 
 git clone --depth 1 $repo $fish
-set -l commit (git -C $fish rev-parse HEAD)
+set --local commit (git -C $fish rev-parse HEAD)
 
 rm -r completions
 mkdir completions
 
 # Builtins should always use vendored completions
-set -l skip (builtin -n)
+set --local skip (builtin --names)
 
 # These are placeholders and will invalidate actual completions
 # https://github.com/fish-shell/fish-shell/blob/master/share/completions/brew.fish
 # https://github.com/fish-shell/fish-shell/blob/master/share/completions/docker.fish
-set -a skip brew docker
+set --append skip brew docker
 
 for completion in $share/completions/*.fish
-    set -l name (path basename $completion)
+    set --local name (path basename $completion)
     # Skip if the completion is in skip list or has a corresponding vendored function
     if ! contains (path change-extension "" $name) $skip && ! test -f $share/functions/$name
         cp $completion completions
@@ -25,7 +25,7 @@ for completion in $share/completions/*.fish
 end
 
 for patch in patches/*.patch
-    patch -p2 <$patch
+    patch --strip=2 <$patch
 end
 
 # `test -n` doesn't work somehow
@@ -34,6 +34,6 @@ if string length (git status --porcelain)
     git config user.email "github-actions[bot]@users.noreply.github.com"
 
     git add completions
-    git commit -m "Sync with $repo/commit/$commit"
+    git commit --message "Sync with $repo/commit/$commit"
     git push
 end
